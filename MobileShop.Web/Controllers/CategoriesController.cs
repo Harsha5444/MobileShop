bfee6d83b88e75145e1a60b9e80ecc2586fa9882
harshavardhan.r <harshavardhan.r@GSPSFJ9S542>
2025-02-01 19:35:57 +0530

    Add project files.

---
+++ MobileShop.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using MobileShop.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileShop.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        MobileShopDBEntities dBEntities = new MobileShopDBEntities();
+        public ActionResult Index()
+        {
+            return View(dBEntities.Categories.ToList());
+        }
+    }
+}
---
+++ MobileShop.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using MobileShop.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileShop.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        MobileShopDBEntities dBEntities = new MobileShopDBEntities();
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
